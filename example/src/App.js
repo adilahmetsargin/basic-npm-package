a6c7971960af46096402c51706a9baabf6002c10
Adil Ahmet Sargın <ahmet.sargin@westerops.com>
2022-05-07 19:31:48 +0300

    button type added

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-import { ExampleComponent } from 'buttonaas'
+import { ButtonComponent } from 'buttonaas'
 import 'buttonaas/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+
+      <ButtonComponent type='secondary' text="Add" />
+    </>
+  
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ButtonComponent = ({ type, text }) => {
+  if (type === 'primary') {
+    return <button className={styles.primary}>{text}</button>
+  } else if (type === 'dashed') {
+    return <button className={styles.dashed}>{text}</button>
+  } else if (type === 'text') {
+    return <button className={styles.text}>{text}</button>
+  } else if (type === 'link') {
+    return <button className={styles.link}>{text}</button>
+  } else if (type === 'secondary') {
+    return <button className={styles.secondary}>{text}</button>
+  }
+  return <button className={styles.default}>{text}</button>
 }
